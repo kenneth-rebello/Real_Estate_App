5339079a2f2b371fb1bb6407272a022478f7d9f5
Kenneth Rebello <54711676+kenneth-rebello@users.noreply.github.com>
2020-04-17 01:48:37 +0530

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -139,3 +139,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'crcesocial@gmail.com'
 EMAIL_HOST_PASSWORD = 'LaDuodecima'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
